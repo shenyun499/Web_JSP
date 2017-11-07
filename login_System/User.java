048c936b143e08c343e78766840071aa2ee71206
xuestudy <31872945+xuestudy@users.noreply.github.com>
2017-11-07 23:30:51 +0800

    用户信息类

---
+++ login_System/User.java
@@ -0,0 +1,54 @@
+package com.base.demo;
+
+public class User {
+	private String username;
+	
+	private String grade;
+	
+	private String number;
+	
+	private String phone;
+	
+	private String password;
+	
+	public String getUsername() {
+		return this.username;
+	}
+	
+	public String getGrade() {
+		return this.grade;
+	}
+	
+	public String getNumber() {
+		return this.number;
+	}
+	
+	public String getPhone() {
+		return this.phone;
+	}
+	
+	public String getPassword() {
+		return this.password;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public void setGrade(String grade) {
+		this.grade = grade;
+	}
+
+	public void setNumber(String number) {
+		this.number = number;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+}
